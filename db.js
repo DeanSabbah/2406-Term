860d1cc4b486131163d32363ff3041c9c63114fe
Dean Sabbah <deanreynolds10@yahoo.ca>
2024-03-17 04:35:47 -0400

    Git ignore + atlas

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 //connects to database. Allows for connection to be used in routers.
 import mongoose from "mongoose";
 
-await mongoose.connect("mongodb://127.0.0.1:27017/term");
+const uri = "mongodb+srv://deansabbah:PotatoFucker%236@cluster0.6nrq45v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+await mongoose.connect(uri);
 console.log("connected to db")
 
 let db = mongoose.connection;
